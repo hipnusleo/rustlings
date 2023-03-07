52f18dabca748b100f6593cabf9ef6814118bb7b
hipnusleo-yyg <hipnusleo@users.noreply.github.com>
2023-03-07 16:19:12 +0800

    Done "move_semantics"

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,16 +2,18 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+
+
+// This practice is about ownership borrowed
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
+    let mut vec1 = fill_vec(vec0);
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
